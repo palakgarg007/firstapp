6a5892818cf26533d3618a9f29b4c4cf3f54a7b0
Palak Garg <gargpalak32@gmail.com>
2023-07-29 22:56:00 +0530

    Adding cards component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import Heading from './Heading';
 import Bag from './components/Bag';
 import Apples from './components/Apples';
@@ -10,6 +10,7 @@ import Intro2 from './components/Intro2';
 import Intro3 from './components/Intro3';
 import Promo from './components/Promo';
 import Footer from './components/Footer';
+import Cards from './components/Cards';
 
 function App() {
   return(
@@ -17,13 +18,22 @@ function App() {
     //   <Bag children={<Apples color="Red" number="5" />}/>
     //   <Bag children={<Pears friend="Peter" />} />
     // </div>
+
+    // <div>
+    //   <Nav />
+    //   <Intro1 />
+    //   <Intro2 />
+    //   <Intro3 />
+    //   <Promo />
+    //   <Footer />
+    // </div>
+
+
     <div>
-      <Nav />
-      <Intro1 />
-      <Intro2 />
-      <Intro3 />
-      <Promo />
-      <Footer />
+      <h1>Task: Add three Card elements</h1>
+      <Cards h2="First card's h2" h3="First card's h3" />
+      <Cards h2="Second card's h2" h3="Second card's h3" />
+      <Cards h3="Third card's h2" h3="Third card's h3" />
     </div>
   )
 }
